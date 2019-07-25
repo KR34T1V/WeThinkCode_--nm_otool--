8b34963d31e3c2d277303c048a0059237853dde0
m4dm0nk3y <m4dm0nk3y.za@gmail.com>
2019-07-25 22:17:35 +0200

    mmmm not for linux

--- src/start_nm.c
+++ src/start_nm.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   private_includes.h                                 :+:      :+:    :+:   */
+/*   start_nm.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/25 22:05:44 by cterblan          #+#    #+#             */
-/*   Updated: 2019/07/25 22:12:06 by cterblan         ###   ########.fr       */
+/*   Created: 2019/07/25 22:05:35 by cterblan          #+#    #+#             */
+/*   Updated: 2019/07/25 22:08:14 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRIVATE_INCLUDES_H
-# define PRIVATE_INCLUDES_H
+#include "../inc/nm_otool.h"
 
-# include <sys/mman.h>
-# include <sys/stat.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <mach-o/loader.h>
-# include <mach-o/nlist.h>
-
-# include "../lib/ft_printf/inc/export_ft_printf.h"
-
-#endif
+void	start_nm(char *file)
+{
+	
+}
